55b505595a33198a943b22cd1c87ee9cdc8b3435
n1mah <nima.heidari.n@gmail.com>
2023-08-25 23:22:01 +0330

    products (Method:GET,POST) added

--- src/pages/api/products/pages.ts
+++ src/pages/api/products/pages.ts
@@ -1,32 +1,106 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type {NextFetchEvent, NextRequest} from "next/server";
 import { Pool } from "@neondatabase/serverless";
+import {json} from "stream/consumers";
+import zod, {string} from "zod";
+import sqlstring from "sqlstring";
 
 export  const config={
 runtime:"edge",
 };
 
+async function extractBody(req: NextRequest) {
+    if (!req.body)
+        return null;
+    const decoder=new TextDecoder();
+    const reader=req.body.getReader();
+    let body='';
+    while (true){
+        const {done,value}=await reader.read();
+        if (done){
+            try {
+                return JSON.parse(body);
+            }catch (e){
+                console.error(e);
+                return null;
+            }
+        }
+     body = body +decoder.decode(value);
+    }
+}
 
+const schema=zod.object({
+    category:string().max(64).min(1),
+    name:string().max(64).min(1),
+    price:string().max(64).min(1),
+    image:string().max(64).min(1),
+})
+async function SetMethod(req: NextRequest, event: NextFetchEvent){
+    const EBody =await extractBody(req);
+    if (!req.body){
+        return new Response("invalid body", {
+            status: 200,
+        });
+    }
 
-export default async function handler(req: NextRequest, event: NextFetchEvent) {
+    try {
+        const {category,name,price,image}=(EBody);
+        const pool = new Pool({
+            connectionString: process.env.DATABASE_URL,
+        })
+        const sql =sqlstring.format(`
+        INSERT INTO Products (id,category,name,price,image)
+        VALUES (uuid_in(md5(random()::text || random()::text)::cstring),?,?,?,?);
+       `,[
+                category,name,price,image
+            ]
+        );
+        console.log(sql)
+        const {rows} = await pool.query(sql);
 
+        event.waitUntil(pool.end());
+        return new Response(
+            "insert successful - OK", {
+                status: 200,
+            });
+    }catch (e){
+        return new Response(
+            "not valid", {
+                status: 400,
+            });
+    }
+
+}
+async function getMethod(req: NextRequest, event: NextFetchEvent){
+
+    const eBody =await extractBody(req);
+    console.log("extractBody",eBody);
     const pool = new Pool({
         connectionString: process.env.DATABASE_URL,
     })
 
     const sql = `
-        SELECT NOW();
+        SELECT * from products;
     `;
 
     const {rows} = await pool.query(sql);
-    const now =rows[0];
-
+    const firstRow=rows[0];
     event.waitUntil(pool.end());
     return new Response(
         JSON.stringify({
-            now
+            rows
         }), {
             status: 200,
         });
+}
 
+export default async function handler(req: NextRequest, event: NextFetchEvent) {
+ if (req.method=="POST")
+     return SetMethod(req,event);
+ else if (req.method=="GET")
+     return getMethod(req,event)
+
+    return new Response("invalid", {
+        status: 200,
+    });
 }
